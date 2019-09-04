eb77abc32b18faefc600a5fac5c441cd8881fd33
tonysk08 <zarmiento227@gmail.com>
2019-09-04 18:08:38 -0500

    base del problema 2

---
+++ AppLab4/AppLab4/Problema2.vb
@@ -0,0 +1,13 @@
+﻿Public Class Problema2
+    Shared Sub Main()
+
+        Console.WriteLine(vbTab & "----------------------------------")
+        Console.WriteLine(vbTab & "Fabrica de sillas Seminole")
+        Console.WriteLine(vbTab & "----------------------------------")
+        Console.ReadLine()
+
+        Dim empleado As String
+        Dim tarifaHora, salarioDiario, incentivo As Double
+        Dim sillasDelDia As Integer
+    End Sub
+End Class
